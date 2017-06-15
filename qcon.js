890e0f0a9686c0edc5b2b5136197f6231d24a072
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-15 16:27:30 +0800

    Updated client test.

--- qcon.js
+++ qcon.js
@@ -35,6 +35,10 @@
 			"module": "qcon",
 			"author": "Richeve S. Bebedor",
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/qcon.git",
 			"test": "qcon-test.js",
 			"global": true
